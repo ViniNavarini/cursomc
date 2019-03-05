9e585a4a3f1d29a0aeaaefbed7949f5480db4b12
ViniNavarini <vininavarini9@gmail.com>
2019-03-05 20:16:52 -0300

    Cliente, TipoCliente, telefones e enderecos

---
+++ src/main/java/com/vininavarini/cursomc/repositories/EnderecoRepository.java
@@ -0,0 +1,11 @@
+package com.vininavarini.cursomc.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.vininavarini.cursomc.domain.Endereco;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<Endereco, Integer>{
+
+}
