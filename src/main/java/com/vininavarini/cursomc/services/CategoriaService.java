20f045508c2f87adcc11bd9696679d0bf93da810
ViniNavarini <vininavarini9@gmail.com>
2019-03-18 20:01:15 -0300

    Validacao sintatica com Bean validation

--- src/main/java/com/vininavarini/cursomc/services/CategoriaService.java
+++ src/main/java/com/vininavarini/cursomc/services/CategoriaService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Sort.Direction;
 import org.springframework.stereotype.Service;
 
 import com.vininavarini.cursomc.domain.Categoria;
+import com.vininavarini.cursomc.dto.CategoriaDTO;
 import com.vininavarini.cursomc.repositories.CategoriaRepository;
 import com.vininavarini.cursomc.services.exceptions.DataIntegrityException;
 import com.vininavarini.cursomc.services.exceptions.ObjectNotFoundException;
@@ -61,6 +62,10 @@ public class CategoriaService {
 		PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),
 				orderBy);
 		return repo.findAll(pageRequest);
+	}
+	
+	public Categoria fromDTO(CategoriaDTO objDto) {
+		return new Categoria(objDto.getId(), objDto.getNome());
 	}
 	
 }
