48ae0d6965c8cb1b7f50c6046031de53c9288c7a
ViniNavarini <vininavarini9@gmail.com>
2019-07-31 20:42:44 -0300

    Email Html

--- src/main/java/com/vininavarini/cursomc/services/EmailService.java
+++ src/main/java/com/vininavarini/cursomc/services/EmailService.java
@@ -1,5 +1,7 @@
 package com.vininavarini.cursomc.services;
 
+import javax.mail.internet.MimeMessage;
+
 import org.springframework.mail.SimpleMailMessage;
 
 import com.vininavarini.cursomc.domain.Pedido;
@@ -9,4 +11,8 @@ public interface EmailService {
 	void sendOrderConfirmationEmail(Pedido obj);
 
 	void sendEmail(SimpleMailMessage msg);
+	
+	void sendOrderConfirmationHtmlEmail(Pedido obj);
+	
+	void sendHtmlEmail(MimeMessage msg);
 }
