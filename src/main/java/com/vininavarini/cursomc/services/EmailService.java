db4533eb18c9f173468516708c3ab86c108e669d
ViniNavarini <vininavarini9@gmail.com>
2019-08-19 16:47:38 -0300

    Esqueci a senha

--- src/main/java/com/vininavarini/cursomc/services/EmailService.java
+++ src/main/java/com/vininavarini/cursomc/services/EmailService.java
@@ -5,16 +5,19 @@ import javax.mail.internet.MimeMessage;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.stereotype.Service;
 
+import com.vininavarini.cursomc.domain.Cliente;
 import com.vininavarini.cursomc.domain.Pedido;
 
 @Service
 public interface EmailService {
 
-	void sendOrderConfirmationEmail(Pedido obj);
-
-	void sendEmail(SimpleMailMessage msg);
-	
 	void sendOrderConfirmationHtmlEmail(Pedido obj);
 	
 	void sendHtmlEmail(MimeMessage msg);
+	
+	void sendOrderConfirmationEmail(Pedido obj);
+
+	void sendEmail(SimpleMailMessage msg);
+
+	void sendNewPasswordEmail(Cliente cliente, String newPass);
 }
